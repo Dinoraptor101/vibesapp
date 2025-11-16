d41c20123dff72a5d95731a2bae07f0c5e7fa30f
DinoRaptor101 <digitals.organization101@Gmail.com>
2025-11-16 15:34:58 -0500

    Remove extra column from PostsGrid layout for improved responsiveness

--- apps/web-v2/src/features/posts/components/PostsFeed.tsx
+++ apps/web-v2/src/features/posts/components/PostsFeed.tsx
@@ -151,7 +151,7 @@ export function PostsFeed({ className }: PostsFeedProps) {
       />
 
       {/* Posts Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
         {posts.map((post) => (
           <PostCard
             key={post._id}
