92e4ec7fee7f175c75da867e75981e3bebd16571
DinoRaptor101 <digitals.organization101@Gmail.com>
2025-12-09 11:52:01 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vibesapp/sw.js', { scope: '/vibesapp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
